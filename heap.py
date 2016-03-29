34ca4e7181346249f0d190ec24308205fd044e08
Raphael Paiva <raphaeldpaiva@gmail.com>
2016-03-29 16:40:17 -0300

    Extracting monitor logic from heap status readings

---
+++ heap.py
@@ -0,0 +1,75 @@
+#!/usr/bin/python
+
+import monitor
+import jbosscli.jbosscli as jbosscli
+
+def monitor_standalone(cli, args, log):
+    try:
+        used_heap = cli.read_used_heap()[0]
+
+        log.info("heap: %.2f gb", used_heap)
+
+        if (used_heap > args.max_heap):
+           log.warn("Restaring", cli.controller)
+           cli.restart()
+
+    except jbosscli.CliError as e:
+        log.error("An error occurred while monitoring %s", cli.controller)
+        log.exception(e)
+
+def monitor_domain(cli, args, log):
+    instances = cli.instances
+    max_heap_usage = args.max_heap_usage
+
+    for instance in instances:
+        try:
+            instances_to_restart = []
+
+            used_heap, max_heap = cli.read_used_heap(instance.host, instance.name)
+            heap_usage = 100 * (used_heap / max_heap)
+            log.info("%s heap: %.2f gb (out of %.2f - %.2f%%)", instance, used_heap, max_heap, heap_usage)
+
+            if (heap_usage > max_heap_usage):
+                log.critical("%s is critical: %.2f%%", instance, heap_usage)
+                instances_to_restart.append(instance)
+
+            restart_count = len(instances_to_restart)
+            if (restart_count > 0):
+                log.info("Restarting %i instances", restart_count)
+                for instance in instances_to_restart:
+                    log.critical("Restarting %s...", instance)
+                    cli.restart(instance.host, instance.name)
+
+        except jbosscli.CliError as e:
+            log.error("An error occurred while monitoring %s", instance)
+            log_exception(e)
+
+
+mon = monitor.Monitor()
+parser = mon.arg_parser
+
+default_max_heap       = 3.5
+default_max_heap_usage = 95
+default_auth           = "jboss:jboss@123"
+
+parser.add_argument("--max-heap",
+                          help="The value in gb in which to take action. If in domain mode, use --max-heap-usage.",
+                          type=float,
+                          default=default_max_heap)
+
+parser.add_argument("--max-heap-usage",
+                    help="The percentage of heap usage in which to take action i.e. --max-heap-usage 95 for a 95%% threshold. If in standalone mode, use --max-heap.",
+                    type=float,
+                    default=default_max_heap_usage)
+
+parser.add_argument("--auth",
+                    help="Authorization key in the format username:password to be used with jboss web interface authentication.",
+                    default=default_auth)
+
+mon.prepare()
+
+cli = jbosscli.Jbosscli(mon.args.controller, mon.args.auth)
+
+func = monitor_domain if cli.domain else monitor_standalone
+
+mon.monitor(func, cli)
---
+++ monitor.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python
+import argparse
+import logging
+import logging.handlers
+import socket
+import sys
+import jbosscli.jbosscli as jbosscli
+import tempfile
+import os
+from time import sleep
+
+class Monitor(object):
+    def __init__(self):
+        self.arg_parser = argparse.ArgumentParser(description="Monitors through loop and sleep.")
+        self.log = None
+
+    def prepare(self):
+        self.args = self._parse_args()
+        self._config_log(self.args.controller, self.args.debug)
+
+    def _config_log(self, controller, debug):
+        log_filename = tempfile.gettempdir() + os.sep + "monitor-heap.log" if not debug else None;
+        log_format = "%(asctime)s [%(name)s] %(levelname)s: %(message)s"
+        log_date_format = "%d/%m/%Y %H:%M:%S"
+
+        logging.basicConfig(format=log_format,
+                            filename=log_filename,
+                            datefmt=log_date_format,
+                            level=logging.INFO if not debug else logging.DEBUG)
+
+        self.log = logging.getLogger("monitor-heap ({0})".format(controller))
+
+        if not debug:
+            formatter = logging.Formatter(log_format, log_date_format)
+            handler = logging.handlers.TimedRotatingFileHandler(log_filename, when='midnight')
+            handler.setFormatter(formatter)
+            handler.setLevel(logging.INFO)
+
+            self.log.propagate = False
+            self.log.addHandler(handler)
+
+        logging.getLogger("requests.packages.urllib3").setLevel(logging.WARNING)
+
+    def _parse_args(self):
+        default_hostname       = socket.gethostname() + ":9990"
+        default_sleep_interval = 300
+
+        self.arg_parser.add_argument("--sleep-interval",
+                            help="The time in seconds between reads",
+                            type=int,
+                            default=default_sleep_interval)
+
+        self.arg_parser.add_argument("--debug",
+                            help="Prints debug logging to sysout",
+                            action="store_true")
+
+        self.arg_parser.add_argument("--controller",
+                            help="The controller to connect to in the hostname:port format",
+                            default=default_hostname)
+
+        return self.arg_parser.parse_args()
+
+    def monitor(self, f, cli):
+        self.log.info("Monitoring controller: %s; args: %s", cli.controller, self.args)
+        sleep_interval = self.args.sleep_interval
+
+        while(True):
+            f(cli, self.args, self.log)
+            sleep(sleep_interval)
+
+    def log_exception(e):
+        log.error(e.msg)
+        log.error(e.raw)
+        log.exception(e)
